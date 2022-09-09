b642a4924f7adabfee4227fb9ab1961409dc5774
God-winss <ownibigodwin@gmail.com>
2022-09-09 13:22:31 -0700

    c programing language 1

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include<stdio>
+/**
+ * main - printing in c
+ *
+ * return: Always 0 (SUCCESS)
+ */
+int main(void)
+{
+	puts("\"programming is like building a multilingual puzzel");
+	return (0);
+}

c50fa10847177b73fceef2391bc91d1625314ca9
God-winss <ownibigodwin@gmail.com>
2022-09-13 03:08:43 -0700

    first commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * return: Always (success)
+ */
+int main(void)
+{
+	char c;
+	for (c='a';c <= 'Z'; c++)
+	{
+		putchar(c);
+	}
+        for (c = 'A'; c <= 'Z'; c++)
+	{
+		putchar(c)
+	}
+	putchar("\n");
+	return (0);
+}

8c52ec676a89e44e17de38b55e3b6813a58767ec
God-winss <ownibigodwin@gmail.com>
2022-09-13 03:42:48 -0700

    first commit

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,14 +7,10 @@
 int main(void)
 {
 	char c;
+	
 	for (c='a';c <= 'Z'; c++)
-	{
 		putchar(c);
-	}
-        for (c = 'A'; c <= 'Z'; c++)
-	{
-		putchar(c)
-	}
-	putchar("\n");
+
+	putchar('\n');
 	return (0);
 }

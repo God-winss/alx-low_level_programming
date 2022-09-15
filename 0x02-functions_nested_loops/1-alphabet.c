cc4f5810e2484c1887a16fa9420be064e907abd2
God-winss <ownibigodwin@gmail.com>
2022-09-15 13:15:02 -0700

    final commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *  print_alphabet - print alphabet
+ */
+{
+	char a = 'a';
+
+	while (a <= 'z')
+	{
+		_putchar(a);
+		++a
+	}
+
+	_putchar(10);
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - function
+ *
+ * @a: parameter 1
+ *
+ * @b: parameter 6
+ *
+ * Return: return sum
+ */
+int add(int a, int b)
+{
+		return (a + b);
+}
